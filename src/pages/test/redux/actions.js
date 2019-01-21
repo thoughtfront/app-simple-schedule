9eaff6025bfe669741b0dc0c8c40ae6810bae4f3
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-20 22:06:00 -0600

    test request action

--- src/pages/test/redux/actions.js
+++ src/pages/test/redux/actions.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
-import constants from '../constants';
+import constants from './constants';
+import config from '../../../configuration';
+
+const buildURL = path => {
+  return `${config.apiHost}/${path}`;
+}
 
 export const testRequest = () => ({
   type: constants.TEST_REQUEST,
-  payload: axios.get('http://local.api.simpleschedule.com:3000/test'),
+  payload: axios.get(buildURL('test')),
 });
