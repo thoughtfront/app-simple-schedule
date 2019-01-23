e69afc93064684581b12ba37736af716cf402a46
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-23 15:37:53 -0600

    api url base built in to axios

--- src/pages/test/redux/actions.js
+++ src/pages/test/redux/actions.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 import constants from './constants';
-import config from '../../../configuration';
-
-const buildURL = path => {
-  return `${config.apiHost}/${path}`;
-}
 
 export const testRequest = () => ({
   type: constants.TEST_REQUEST,
-  payload: axios.get(buildURL('test')),
+  payload: axios.get('test', {withCredentials: true}),
 });
